c1e2f493d11464c724592ca759b21fce6bc9f2da
Roberto <161033388+Hubierto@users.noreply.github.com>
2024-03-26 10:30:57 -0300

    Add files via upload

---
+++ limguagem c - 26.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<locale.h>
+
+int main () {
+	setlocale(LC_ALL,"");
+	
+	float nota, soma = 0, media;
+	int i;
+	
+	for (i = 1; i <= 2; i++) {
+		do { 
+		    printf("digite a %iª nota: ", i);
+		    scanf("%f",&nota);
+		} while (nota < 0 || nota > 10);
+		
+		soma += nota;
+	}
+	
+	media = soma/2;
+	
+	printf("\n === Exibindo resultados ===\n");
+	printf("Média: %.1f \n", media);
+	
+	return 0;
+}
